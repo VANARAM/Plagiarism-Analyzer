1f18f98d1ef40dbc67af70062172773068fe7e11
VANARAM <95128391+VANARAM@users.noreply.github.com>
2022-06-01 07:50:37 +0530

    StudentUpdate
    Student Module have been updated.

---
+++ user/includes/footer.php
@@ -0,0 +1,7 @@
+ <div class="row">
+                        <div class="col-lg-12">
+                            <div class="footer">
+                                <p>Plagairism Analyzer</p>
+                            </div>
+                        </div>
+                    </div>
